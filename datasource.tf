deeb403aa27ef370a021a79f21d4fc0ba5719a12
Shaik-Basha68 <oops.1294733@gmail.com>
2026-01-19 12:24:43 +0530

    Add aws_route_table data source for main route table
    Added aws_route_table data source to reference the main route table associated with the selected VPC.

--- datasource.tf
+++ datasource.tf
@@ -4,5 +4,13 @@
 
 data "aws_vpc" "selected" {
   default=true
-
 }
+
+data "aws_route_table" "main" {
+  vpc_id = data.aws_vpc.selected.id
+  filter{
+    name = "association.main"
+    values = ["true"] 
+  }
+}
+
