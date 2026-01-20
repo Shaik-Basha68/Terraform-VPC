c3372030e50baf0c5ae10bcdc08be7dbd7aebbdd
Shaik-Basha68 <oops.1294733@gmail.com>
2026-01-20 15:11:25 +0530

    Add is_peering_required parameter to VPC module

--- vpc-module-test/main.tf
+++ vpc-module-test/main.tf
@@ -7,6 +7,8 @@ module "vpc" {
   Environment  = var.Environment
   public_subnet_cidrs = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
+  is_peering_required= true
   
 }
+
 
