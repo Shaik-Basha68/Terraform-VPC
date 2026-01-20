18fdba8a43bf74f75dde6370c6347f7242df16ed
Shaik-Basha68 <oops.1294733@gmail.com>
2026-01-20 15:07:43 +0530

    Add VPC outputs to output.tf

--- vpc-module-test/output.tf
+++ vpc-module-test/output.tf
@@ -1,15 +1,16 @@
-# output "vpc_id" {
-#   description = "The ID of the VPC"
-#   value       = module.vpc.vpc_id
-# }
+ output "vpc_id" {
+   description = "The ID of the VPC"
+   value       = module.vpc.vpc_id
+ }
 
-# output "azs" {
-#   description = "List of availability zones"
-#   value       = data.aws_availability_zones.available.names
+ output "azs" {
+   description = "List of availability zones"
+   value       = data.aws_availability_zones.available.names
 
-# }
+ }
 
-# /* output "default_vpc_id" {
-#   description = "The ID of the default VPC"
-#   value       = data.aws_vpc.selected
-# } */
+ output "default_vpc_id" {
+   description = "The ID of the default VPC"
+   value       = data.aws_vpc.selected
+
+ }
